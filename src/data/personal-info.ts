f6c5a68766a163380a0628443096a2246cbacbd0
Oribel Fonseca <oribelfonseca@gmail.com>
2026-01-06 13:29:20 -0500

    Update personal information data

--- src/data/personal-info.ts
+++ src/data/personal-info.ts
@@ -4,8 +4,9 @@ export const personalInfo = {
   title: "Desarrollador Web Full Stack",
   location: "Pinar del Rio, Cuba",
   email: "oribelfonseca@gmail.com",
-  github: "https://github.com/tuusuario",
-  linkedin: "https://linkedin.com/in/tuusuario",
+  github: "https://github.com/orifons",
+  linkedin: "https://www.linkedin.com/in/oribel-fonseca-acosta-6a68b532b/",
   bio: "Desarrollador web apasionado por crear soluciones innovadoras y experiencias de usuario excepcionales. Especializado en tecnologías modernas y mejores prácticas de desarrollo.",
-  avatar: "https://www.shutterstock.com/image-vector/programmer-computer-expert-black-linear-600nw-2033137370.jpg",
+  avatar:
+    "https://www.shutterstock.com/image-vector/programmer-computer-expert-black-linear-600nw-2033137370.jpg",
 };
